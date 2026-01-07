cceb9e25544189011992be9f2dcc3930749f68fe
jeromefourdrain <jerome.fourdrain@gmail.com>
2026-01-07 10:31:09 +0100

    Ajoutez des fichiers projet.

---
+++ PokIpsoBowl_Blazor/Model/SelectedIngredient.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokIspoBowl_API.Model
+{
+    [Index(nameof(OrderLineId), nameof(IngredientId), IsUnique = true)]
+    public class SelectedIngredient
+    {
+        [Key]
+        public long SelectedIngredientId { get; set; }
+
+        public bool IsExtra { get; set; }
+
+
+        public int BowlId { get; set; }
+        [ForeignKey("BowlId")]
+        public Bowl BowlPreset { get; set; }
+
+        public long OrderLineId { get; set; }
+        [ForeignKey("OrderLineId")]
+        public OrderLine OrderLine { get; set; }
+
+        public int IngredientId { get; set; }
+        [ForeignKey("IngredientId")]
+        public Ingredient Ingredient { get; set; }
+    }
+}
